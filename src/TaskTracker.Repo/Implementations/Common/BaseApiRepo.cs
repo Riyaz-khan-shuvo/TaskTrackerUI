9c5331ac3f4107c53c42560dd79ae8a06c4b7242
Riyaz-khan-shuvo <mdriyaz5965@gmail.com>
2026-04-22 21:55:15 +0600

    task repo has created

---
+++ src/TaskTracker.Repo/Implementations/Common/BaseApiRepo.cs
@@ -0,0 +1,41 @@
+﻿using AutoMapper;
+using TaskTracker.Models;
+using TaskTracker.Models.kendocommon;
+using TaskTracker.Shared.ApiClients;
+
+namespace TaskTracker.Repo.Implementations.Common
+{
+    public abstract class BaseApiRepo
+    {
+        protected readonly IMapper _mapper;
+
+        protected BaseApiRepo(IMapper mapper)
+        {
+            _mapper = mapper;
+        }
+
+        protected TQuery MapGridQuery<TQuery>(GridOptionVM options)
+            where TQuery : GridOptions, new()
+        {
+            var baseQuery = _mapper.Map<GridOptions>(options);
+            return _mapper.Map<TQuery>(baseQuery);
+        }
+
+        // Map NSwag ResultVM → your internal ResultViewModel
+        protected ResultViewModel MapResult(ResultVM result)
+        {
+            return _mapper.Map<ResultViewModel>(result);
+        }
+
+        // Generic executor: call any service method with a TQuery
+        protected async Task<ResultViewModel> ExecuteGridAsync<TQuery>(
+            GridOptionVM options,
+            Func<TQuery, Task<ResultVM>> apiCall)
+            where TQuery : GridOptions, new()
+        {
+            var query = MapGridQuery<TQuery>(options);
+            var result = await apiCall(query);
+            return MapResult(result);
+        }
+    }
+}
---
+++ src/TaskTracker.Repo/Implementations/TaskRepo.cs
@@ -0,0 +1,47 @@
+﻿using AutoMapper;
+using TaskTracker.Models;
+using TaskTracker.Models.kendocommon;
+using TaskTracker.Repo.Implementations.Common;
+using TaskTracker.Shared.ApiClients;
+
+namespace TaskTracker.Repo.Implementations
+{
+    public class TaskRepo : BaseApiRepo
+    {
+        private readonly TaskService _taskService;
+
+        public TaskRepo(TaskService taskService, IMapper mapper)
+            : base(mapper)
+        {
+            _taskService = taskService;
+        }
+
+        public async Task<ResultViewModel> GetGridDataAsync(GridOptionVM options)
+        {
+            return await ExecuteGridAsync<GetTaskGridQuery>(
+                options,
+                query => _taskService.GetGridDataAsync(query)
+            );
+        }
+
+        public async Task<ResultVM> UpsertAsync(TaskItemModel vm)
+        {
+            var command = _mapper.Map<UpsertTaskCommand>(vm);
+            return await _taskService.UpsertAsync(command);
+        }
+
+        public async Task<ResultVM> GetListAsync(int id)
+        {
+            return await _taskService.GetAsync(id);
+        }
+
+        public async Task<ResultVM> DeleteAsync(IEnumerable<string> ids)
+        {
+            var command = new DeleteTaskCommand
+            {
+                IDs = ids.ToList()
+            };
+            return await _taskService.DeleteAsync(command);
+        }
+    }
+}
